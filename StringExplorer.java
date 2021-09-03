ca402cda3571ea41772d0c09630c725446c4baa1
Matt Callaghan <mcallaghan@naperville203.org>
2021-09-03 13:12:10 -0500

    finished String Explorer class

--- StringExplorer.java
+++ StringExplorer.java
@@ -2,13 +2,42 @@
 /**
  * Exploring the String class and its methods.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author mcallaghan
+ * @version 03sept2021
  */
 public class StringExplorer
 {
     public static void main(String[] args)
     {
+        String river = new String("Mississippi");
+        String river2 = "Mississippi";  // string literal
+        river = "Nile";
+        System.out.print(river);
+        System.out.println(river2);
+        
+        
+        river.replace("i", "x");
+        System.out.println(river);
+        
+        /*
+         * The replace method returns a reference to a new String.
+         *  It does not change the String object on which it is invoked.
+         *  
+         *  The String class has to mutator methods.  It is not possible to change a String object.
+         *  
+         *  String are immutable.
+         */
+        
+        String riverX = river.replace("i", "x");
+        System.out.println(riverX);
+        System.out.println(river);
         
     }
+    
+    
+    
+    
+    
+    
+    
 }
